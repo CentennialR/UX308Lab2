7d5944e35417ce1c2e823eae2abf3d4f0d25b748
CentennialR <145622034+CentennialR@users.noreply.github.com>
2025-01-17 01:51:47 +0000

    Add 3 files for lab 2

---
+++ cylindervolume.js
@@ -0,0 +1,9 @@
+// This program *calculate* the **diameter** of a cylinder's **height** in meters
+
+let diameter  = 30;
+
+let height  = 60;
+
+let radius = diameter/2;
+
+console.log(`A diameter of ${diameter} gives a radius ${radius} and a height of ${height} meters`);
---
+++ numlettergrade.js
@@ -0,0 +1,7 @@
+// This program *converts* **numeric grades** to **letter grades**
+
+let numeric  = 90;
+
+let letter  = "A";
+
+console.log(`${numeric} as a lettered grade is ${letter}`);
